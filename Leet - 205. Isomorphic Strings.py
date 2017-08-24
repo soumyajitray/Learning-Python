7e3838359e2a28c99bae04c5f42470d503773a1e
Soumyajit Ray <gate2soumyajit@gmail.com>
2017-08-24 12:15:57 -0400

    python
    python

---
+++ Leet - 205. Isomorphic Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)== len(t):
+            
+        else:
+            return False
